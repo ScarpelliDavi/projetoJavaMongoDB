a1263d36fbb0e93749078f20fcf3b0f5ffb2d53e
ScarpelliDavi <daviscarpelli8@gmail.com>
2025-08-29 14:16:18 -0300

    primeiro commit

---
+++ src/main/java/com/Davi/projetoJavaMongoDB/ProjetoJavaMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.Davi.projetoJavaMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJavaMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJavaMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Davi/projetoJavaMongoDB/ProjetoJavaMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Davi.projetoJavaMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
