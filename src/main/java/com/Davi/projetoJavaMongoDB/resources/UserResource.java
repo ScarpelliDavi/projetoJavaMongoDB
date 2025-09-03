228b0ea4e5dc33ddc1a6b4685e49954d286cec7f
ScarpelliDavi <daviscarpelli8@gmail.com>
2025-09-03 14:25:45 -0300

    endpoint retornando os posts de um user

--- src/main/java/com/Davi/projetoJavaMongoDB/resources/UserResource.java
+++ src/main/java/com/Davi/projetoJavaMongoDB/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.Davi.projetoJavaMongoDB.resources;
 
+import com.Davi.projetoJavaMongoDB.domain.Post;
 import com.Davi.projetoJavaMongoDB.domain.User;
 import com.Davi.projetoJavaMongoDB.dto.UserDTO;
 import com.Davi.projetoJavaMongoDB.services.UserService;
@@ -54,4 +55,11 @@ public class UserResource {
         user = userService.update(user);
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity <List<Post>> findPostsById(@PathVariable String id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user.getPosts());
+    }
+
 }
