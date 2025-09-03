ad2bf9652d7854fc8f9aadb352f4be284b0cb1ee
ScarpelliDavi <daviscarpelli8@gmail.com>
2025-09-03 14:20:23 -0300

    referenciando posts do user

--- src/main/java/com/Davi/projetoJavaMongoDB/config/Instantiation.java
+++ src/main/java/com/Davi/projetoJavaMongoDB/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.Davi.projetoJavaMongoDB.config;
 
 import com.Davi.projetoJavaMongoDB.domain.Post;
 import com.Davi.projetoJavaMongoDB.domain.User;
+import com.Davi.projetoJavaMongoDB.dto.AuthorDTO;
 import com.Davi.projetoJavaMongoDB.repository.PostRepository;
 import com.Davi.projetoJavaMongoDB.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,10 +37,13 @@ public class Instantiation implements CommandLineRunner {
 
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-        Post p1 = new Post(null, sdf.parse("21/03/2018").toInstant(), "Partiu Viagem", "Vou viajar pra Sao Paulo. Abracos!!", maria);
-        Post p2 = new Post(null, sdf.parse("23/03/2018").toInstant(), "Bom dia", "Acordei feliz hoje", maria);
+        Post p1 = new Post(null, sdf.parse("21/03/2018").toInstant(), "Partiu Viagem", "Vou viajar pra Sao Paulo. Abracos!!", new AuthorDTO(maria));
+        Post p2 = new Post(null, sdf.parse("23/03/2018").toInstant(), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(p1,p2));
+
+        maria.getPosts().addAll(Arrays.asList(p1,p2));
+        userRepository.save(maria);
 
     }
 }
